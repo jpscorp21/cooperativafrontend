c209fc935963bc18220560cfde2e3a78a08d6902
cristiojeda <maria-almeida1966@hotmail.com>
2021-04-29 21:10:32 -0400

    Formulario Tipo Garantia Terminado

--- src/containers/TipoGarantia/TipoGarantia.tsx
+++ src/containers/TipoGarantia/TipoGarantia.tsx
@@ -1,16 +1,69 @@
-import { useState } from "react";
+import { Box, Button, TextField } from "@material-ui/core"
+import { useState, useMemo } from "react";
+import AccionesCell from "../../components/AccionesCell";
+import { useQuery } from "react-query";
+import CustomTable, { ColumnCustomTable } from "../../components/CustomTable";
+import { tipogarantias } from "../../api/tipogarantias";
+import TituloContainer from "../../components/TituloContainer";
 import ButtonActionContainer from "../../components/ButtonActionContainer";
-import TituloContainer from "../../components/TituloContainer"
+import TipoGarantiaFormModal from "./TipoGarantiaFormModal";
+
+const useTipoGarantia = () => {
+  const {data: items} = useQuery('tipogarantias', tipogarantias.getAll);  
+
+  return items;
+}
 
 const TipoGarantia = () => {
 
+  const items = useTipoGarantia(); 
+
   const [openModal, setOpenModal] = useState(false);
+
+  const handlePageChange = (page: number) => {
+    console.log(page);
+  }
+
+  const handleCloseModal = (e: any) => {
+    setOpenModal(false);
+  }
+  
+  const handleEditar = (item: any) => {
+    console.log({item});
+  }
+  const columns = useMemo(() => [
+    {
+      key: 'codigo',
+      label: 'Codigo',          
+    },
+    {
+      key: 'descripcion',
+      label: 'Descripcion',            
+    },    
+    {
+      key: 'acciones',
+      label: 'Acciones',
+      align: 'right',
+      render: (item: any) => <AccionesCell item={item} onEditar={handleEditar} />
+    },
+  ] as ColumnCustomTable[], [])
 
   return (
     <>
       <TituloContainer>Tipo Garantía</TituloContainer>  
 
       <ButtonActionContainer onNew={() => setOpenModal(true)} onRefresh={() => console.log('refrescando')} />            
+
+      <Box px={2} pb={2}>
+        <TextField sx={{bgcolor: 'white'}} fullWidth placeholder="Buscar" size="small" />
+      </Box>           
+
+      {/* TABLA */}
+      <Box sx={{px: 2}}>
+        <CustomTable columns={columns} data={items} onPageChange={handlePageChange}></CustomTable>
+      </Box> 
+      <TipoGarantiaFormModal openModal={openModal} handleCloseModal={handleCloseModal}></TipoGarantiaFormModal>
+
     </>
   )
 }
---
+++ src/containers/TipoGarantia/TipoGarantiaFormModal.tsx
@@ -0,0 +1,36 @@
+import { Box, Button, Dialog, Grid, Paper, TextField, Typography } from "@material-ui/core"
+
+type TipoGarantiaFormModalProps = {
+  openModal : boolean;
+  handleCloseModal(e: any): void;
+}
+const TipoGarantiaFormModal = ({openModal, handleCloseModal}: TipoGarantiaFormModalProps) => {
+  return (
+    <Dialog open={openModal} onClose={handleCloseModal}>
+        <Paper elevation={6} sx={{p: 2}}>
+          <Typography variant="h5" component="h5">
+            Formulario Tipo garantía
+          </Typography>          
+          <form>
+            <Grid container sx={{mt:2}} spacing={2}>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Código" name="codigo" size="small" disabled />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Descripción" name="descripcion" size="small" autoFocus />
+              </Grid>
+              
+              <Grid item xs={12}>
+                <TextField fullWidth label="Observación" multiline name="obvervacion" size="small" rows={4}/>
+              </Grid>            
+            </Grid>
+            <Box sx={{pt: 4, textAlign: 'center'}}>
+              <Button variant="contained" fullWidth color="secondary">Guardar cambios</Button>
+            </Box>
+          </form>
+        </Paper>
+      </Dialog> 
+  )
+}
+
+export default TipoGarantiaFormModal;
