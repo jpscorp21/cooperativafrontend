dd6d74d494c01fc2f128a43fd3d638495d09f2ee
cristiojeda <maria-almeida1966@hotmail.com>
2021-04-29 21:50:51 -0400

    Nueva Modificacion de Nacionalidades Terminado

--- src/api/NacionalidadesAPI.ts
+++ src/api/NacionalidadesAPI.ts
@@ -1,3 +1,49 @@
-export const NacionalidadesAPI = {
-    
+import api from "."
+import { INacionalidad } from "../models/nacionalidad-model";
+
+export const NacionalidadAPI = {
+
+    key: 'nacionalidad-model',
+
+    async getAll(params: any) {
+        try {            
+            const { data, currentPage, pageSize, totalPages}: any = await api.get<INacionalidad[]>('nacionalidad-model', {params});            
+            
+            return {items: data as INacionalidad[], currentPage, pageSize, totalPages } as any;
+        } catch(e) {
+            return []
+        }
+    },
+
+    async create(body: any) {
+        try {            
+            const { data }: any = await api.post<INacionalidad[]>('nacionalidad-model', body);            
+            
+            return {items: data as INacionalidad[] };
+        } catch(e) {
+            console.log(e);
+            throw new Error(e);            
+        }
+    },
+
+    async update(body: any, id: string) {
+        try {            
+            await api.put<INacionalidad[]>('nacionalidad-model/' + id, body);            
+            
+            return {items: body};
+        } catch(e) {
+            console.log(e);
+            throw new Error(e);            
+        }
+    },
+
+    async remove(id: string) {        
+        await api.delete<INacionalidad[]>('nacionalidad-model/' + id);            
+        return true;
+    },
+
+    async getById(id: any) {
+        const { data } = await api.get<INacionalidad>('nacionalidad-model', {params: {id}});
+        return data;        
+    }
 }
--- src/containers/Nacionalidades/Nacionalidades.tsx
+++ src/containers/Nacionalidades/Nacionalidades.tsx
@@ -1,38 +1,76 @@
-import { Box, TextField } from "@material-ui/core";
-import { useQuery } from "react-query";
-import { useMemo, useState } from "react";
-import { nacionalidades } from "../../api/nacionalidades";
-import AccionesCell from "../../components/AccionesCell";
+import { Box, TableCell, TextField } from "@material-ui/core"
+import { useMemo, useState } from "react"
 import CustomTable, { ColumnCustomTable } from "../../components/CustomTable";
+import AccionesCell from "../../components/AccionesCell";
 import TituloContainer from "../../components/TituloContainer";
 import ButtonActionContainer from "../../components/ButtonActionContainer";
 import NacionalidadesFormModal from "./NacionalidadesFormModal";
-
-const useNacionalidades = () => {
-  const {data: items} = useQuery('nacionalidades', nacionalidades.getAll);  
-
-  return items;
-}
-
-// Initial Form
+import { FormApi } from "final-form";
+import queryClient from "../../config/queryClient";
+import ConfirmDialog from "../../components/ConfirmDialog";
+import { NacionalidadesAPI } from "../../api/NacionalidadesAPI";
+import useBackend from "../../shared/hooks/useBackend";
+const initialForm = () => ({
+  descripcion: '',
+  observacion: ''
+}) 
 
 const Nacionalidades = () => {
 
-  const items = useNacionalidades(); 
+  const {data, create, remove, update, setParams, key} = useBackend(NacionalidadesAPI);
 
-  const [openModal, setOpenModal] = useState(false)  
-
-  const handlePageChange = (page: number) => {
-    console.log(page);
+  const [openModal, setOpenModal] = useState(false)
+  const [openConfirmModal, setOpenConfirmModal] = useState(false) 
+  const [formData, setFormData] = useState<any>(initialForm()); 
+  const handleNew = () => {
+    setFormData(initialForm());
+    setOpenModal(true);
   }
 
   const handleEditar = (item: any) => {
-    console.log({item});
+    setFormData({...item});
+    setOpenModal(true);
   }
 
-  const handleCloseModal = (e: any) => {
-    setOpenModal(false);
+  const handleOpenConfirmEliminar = (item: any) => {
+    setFormData({...item});
+    setOpenConfirmModal(true);
   }
+
+  const handleEliminar = () => {
+    remove.mutate(formData.id, {
+      onSuccess() {      
+        setOpenConfirmModal(false);      
+        queryClient.invalidateQueries(key)           
+      }
+    }) 
+  }
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  }  
+
+  const onSubmit = async (values: any, form: FormApi) => {   
+        
+    if (values.id) {
+      update.mutate(({body: values, id: values.id}), {
+        onSuccess() {    
+          handleCloseModal();     
+          queryClient.invalidateQueries(key)   
+          form.reset();
+        }
+      }) 
+      return;
+    }
+
+    create.mutate(values, {
+      onSuccess() {    
+        handleCloseModal();     
+        queryClient.invalidateQueries(key)   
+        form.reset();
+      }
+    })    
+  }    
 
   const columns = useMemo(() => [
     {
@@ -42,12 +80,21 @@ const Nacionalidades = () => {
     {
       key: 'descripcion',
       label: 'Descripcion',            
-    },    
+      render: (item: any) => (
+        <TableCell>
+          <span style={{cursor: 'pointer', paddingTop: '8px'}} onClick={() => handleEditar(item)}>{item.descripcion}</span>
+        </TableCell>
+      )
+    },        
+    {
+      key: 'observacion',
+      label: 'Observación',                  
+    },        
     {
       key: 'acciones',
       label: 'Acciones',
       align: 'right',
-      render: (item: any) => <AccionesCell item={item} onEditar={handleEditar} />
+      render: (item: any) => <AccionesCell item={item} onEditar={handleEditar} onEliminar={handleOpenConfirmEliminar} />
     },
   ] as ColumnCustomTable[], [])
   
@@ -59,16 +106,33 @@ const Nacionalidades = () => {
 
       <ButtonActionContainer onNew={() => setOpenModal(true)} onRefresh={() => console.log('refrescando')} />                
 
-      <Box px={2} pb={2}>
-        <TextField sx={{bgcolor: 'white'}} fullWidth placeholder="Buscar" size="small" />
-      </Box>     
-
-      {/* TABLA */}
+      <Box px={2} pb={2}> 
+        <TextField sx={{bgcolor: 'white'}} onChange={(event) => setParams(event.target.value, 'searchQuery')} fullWidth placeholder="Buscar una ciudad" size="small" />
+      </Box>                
       <Box sx={{px: 2}}>
-        <CustomTable columns={columns} data={items} onPageChange={handlePageChange}></CustomTable>
+        <CustomTable 
+          page={data?.currentPage}  
+          count={data?.totalPages} 
+          columns={columns} 
+          data={data?.items ? data?.items : []} 
+          onPageChange={(value) => setParams(value, 'pageNumber')}
+        />  
       </Box> 
 
-      <NacionalidadesFormModal openModal={openModal} handleCloseModal={handleCloseModal}></NacionalidadesFormModal>
+      <NacionalidadesFormModal                                                          
+        openModal={openModal}
+        handleCloseModal={handleCloseModal}
+        onSubmit={onSubmit}
+        formData={formData}
+      />
+
+      <ConfirmDialog 
+        openModal={openConfirmModal}
+        onAceptar={handleEliminar}
+        message="Estás seguro de eliminar está ciudad?"
+        handleCloseModal={() => setOpenConfirmModal(false)}
+      />
+
     
   </>
   )
