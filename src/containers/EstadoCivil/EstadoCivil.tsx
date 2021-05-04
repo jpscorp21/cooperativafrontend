c8d3222924ca541b879e10eede5fe62bb76ca42d
cristiojeda <maria-almeida1966@hotmail.com>
2021-05-04 17:05:51 -0400

    Estados Civiles terminado

--- src/containers/EstadoCivil/EstadoCivil.tsx
+++ src/containers/EstadoCivil/EstadoCivil.tsx
@@ -1,37 +1,77 @@
-import { Box, TextField } from "@material-ui/core"
-import { useQuery } from "react-query";
+import { Box, TableCell, TextField } from "@material-ui/core"
 import { useMemo, useState } from "react"
 import AccionesCell from "../../components/AccionesCell";
 import CustomTable, { ColumnCustomTable } from "../../components/CustomTable";
-import { estadosciviles } from "../../api/estadosciviles";
 import TituloContainer from "../../components/TituloContainer";
 import ButtonActionContainer from "../../components/ButtonActionContainer";
 import EstadoCivilFormModal from "./EstadoCivilFormModal";
+import { FormApi } from "final-form";
+import queryClient from "../../config/queryClient";
+import ConfirmDialog from "../../components/ConfirmDialog";
+import useBackend from "../../shared/hooks/useBackend";
+import { EstadosCivilesAPI } from "../../api/EstadosCivilesAPI";
 
-
-const useEstadosCiviles = () => {
-  const {data: items} = useQuery('estadosciviles', estadosciviles.getAll);  
-
-  return items;
-}
+const initialForm = () => ({
+  descripcion: '',
+  observacion: ''
+})
 
 const EstadoCivil = () => {
 
-  const items = useEstadosCiviles(); 
+  const {data, create, remove, update, setParams, key} = useBackend(EstadosCivilesAPI);
 
-  const [openModal, setOpenModal] = useState(false)  
-
-  const handlePageChange = (page: number) => {
-    console.log(page);
+  const [openModal, setOpenModal] = useState(false)
+  const [openConfirmModal, setOpenConfirmModal] = useState(false) 
+  const [formData, setFormData] = useState<any>(initialForm()); 
+  const handleNew = () => {
+    setFormData(initialForm());
+    setOpenModal(true);
   }
 
   const handleEditar = (item: any) => {
-    console.log({item});
+    setFormData({...item});
+    setOpenModal(true);
   }
 
-  const handleCloseModal = (e: any) => {
-    setOpenModal(false);
+  const handleOpenConfirmEliminar = (item: any) => {
+    setFormData({...item});
+    setOpenConfirmModal(true);
   }
+
+  const handleEliminar = () => {
+    remove.mutate(formData.id, {
+      onSuccess() {      
+        setOpenConfirmModal(false);      
+        queryClient.invalidateQueries(key)           
+      }
+    }) 
+  }
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  }  
+
+  const onSubmit = async (values: any, form: FormApi) => {   
+        
+    if (values.id) {
+      update.mutate(({body: values, id: values.id}), {
+        onSuccess() {    
+          handleCloseModal();     
+          queryClient.invalidateQueries(key)   
+          form.reset();
+        }
+      }) 
+      return;
+    }
+
+    create.mutate(values, {
+      onSuccess() {    
+        handleCloseModal();     
+        queryClient.invalidateQueries(key)   
+        form.reset();
+      }
+    })    
+  }    
 
   const columns = useMemo(() => [
     {
@@ -41,12 +81,21 @@ const EstadoCivil = () => {
     {
       key: 'descripcion',
       label: 'Descripcion',            
-    },    
+      render: (item: any) => (
+        <TableCell>
+          <span style={{cursor: 'pointer', paddingTop: '8px'}} onClick={() => handleEditar(item)}>{item.descripcion}</span>
+        </TableCell>
+      )
+    },        
+    {
+      key: 'observacion',
+      label: 'Observación',                  
+    },        
     {
       key: 'acciones',
       label: 'Acciones',
       align: 'right',
-      render: (item: any) => <AccionesCell item={item} onEditar={handleEditar} />
+      render: (item: any) => <AccionesCell item={item} onEditar={handleEditar} onEliminar={handleOpenConfirmEliminar} />
     },
   ] as ColumnCustomTable[], [])
   
@@ -59,19 +108,31 @@ const EstadoCivil = () => {
 
       <ButtonActionContainer onNew={() => setOpenModal(true)} onRefresh={() => console.log('refrescando')} />        
       
-
-      <Box px={2} pb={2}>
-        <TextField sx={{bgcolor: 'white'}} fullWidth placeholder="Buscar" size="small" />
-      </Box>     
-
-      {/* TABLA */}
+      <Box px={2} pb={2}> 
+        <TextField sx={{bgcolor: 'white'}} onChange={(event) => setParams(event.target.value, 'searchQuery')} fullWidth placeholder="Buscar un Estado Civil" size="small" />
+      </Box>                
       <Box sx={{px: 2}}>
-        <CustomTable columns={columns} data={items} onPageChange={handlePageChange}></CustomTable>
+        <CustomTable 
+          page={data?.currentPage}  
+          count={data?.totalPages} 
+          columns={columns} 
+          data={data?.items ? data?.items : []} 
+          onPageChange={(value) => setParams(value, 'pageNumber')}
+        />  
       </Box> 
-
-
-      <EstadoCivilFormModal openModal={openModal} handleCloseModal={handleCloseModal}></EstadoCivilFormModal>
+      <EstadoCivilFormModal
+         openModal={openModal}
+         handleCloseModal={handleCloseModal}
+         onSubmit={onSubmit}
+         formData={formData}
+         />
     
+      <ConfirmDialog 
+        openModal={openConfirmModal}
+        onAceptar={handleEliminar}
+        message="Estás seguro de eliminar este Estado Civil?"
+        handleCloseModal={() => setOpenConfirmModal(false)}
+      />
     </>
   )
 }
--- src/containers/EstadoCivil/EstadoCivilFormModal.tsx
+++ src/containers/EstadoCivil/EstadoCivilFormModal.tsx
@@ -1,39 +1,56 @@
 import { Box, Button, Dialog, Grid, Paper, TextField, Typography } from "@material-ui/core";
-import { FormModalProps } from "../../types";
+import { Field, Form } from "react-final-form";
+import { required } from "../../utils/errorMessages";
+import TextFieldAdapter from "../../components/control/TextFieldAdapter";
 
+type EstadoCivilFormModalProps = {
+  openModal : boolean;
+  handleCloseModal(e: any): void; 
+} & {
+  onSubmit: any;
+  formData: any;  
+}
 
-const EstadoCivilFormModal = ({openModal, handleCloseModal}: FormModalProps) => {
-    return (
+const EstadoCivilFormModal = ({openModal, handleCloseModal}: EstadoCivilFormModalProps) => {
+    
+  const onSubmit = async (values:any) => {
+    console.log(values);
+  }
+
+  return (
         <Dialog open={openModal} onClose={handleCloseModal}>
         <Paper elevation={6} sx={{p: 2}}>
         
           <Typography variant="h5" component="h5" sx={{pb: 2}}>
             Formulario Estado Civil
-          </Typography>          
-          <form>
-            <Grid container sx={{mt:2}} spacing={2}>
+          </Typography> 
 
-              <Grid item xs={12}>
-                <TextField fullWidth label="Código" name="codigo" size="small" disabled />
-              </Grid>
-              
-              <Grid item xs={12}>
-                <TextField fullWidth label="Descripción" name="descripcion" size="small" autoFocus />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField fullWidth label="Observación" multiline name="observacion" size="small" rows={4}/>
-              </Grid>            
-            </Grid>
-
+          <Form
+            initialValues={{}} 
+            onSubmit={onSubmit}
+            render={({handleSubmit, values}) => (
+              <form onSubmit={handleSubmit}>
+                 {/* {JSON.stringify(values)}  */}
+                <Grid container sx={{mt:2}} spacing={2}>
+                  <Grid item xs={12}>
+                    <Field fullWidth label="Código" name="codigo" disabled component={TextFieldAdapter} />
+                  </Grid>
+                  <Grid item xs={12}>
+                  <Field fullWidth label="Descripción*" validate={required} name="descripcion" autoFocus component={TextFieldAdapter} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field fullWidth label="Observación" multiline name="observacion" rows={4} component={TextFieldAdapter} />
+                  </Grid>            
+                </Grid>
             <Box sx={{pt: 4, textAlign: 'center'}}>
-              <Button variant="contained" fullWidth color="secondary">Guardar cambios</Button>
-
+              <Button type="submit" variant="contained" fullWidth color="secondary">Guardar cambios</Button>
             </Box>
           </form>
-
-        </Paper>
-      </Dialog>
-    )
+        )}
+      />
+    </Paper>
+  </Dialog>
+  )
 }
 
 export default EstadoCivilFormModal;
